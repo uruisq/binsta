131e4c57ddf59280801bb64105d3f3fc1f2c720f
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-10-15 17:08:37 +0900

    gitignoreに追加

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ Bundler.require(*Rails.groups)
 
 module Binsta
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
 
     config.load_defaults 5.2
 
