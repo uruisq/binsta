bd235a3676e7278af6fc09ca740f624fa0ad55b7
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-12-05 13:12:02 +0900

    環境ファイル変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   config.assets.js_compressor = :uglifier
  
